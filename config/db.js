f26fc341bf2429ad7b35a25398e2d0577115d97b
DaveOrDavid <david.santore@gmail.com>
2019-07-22 13:19:48 -0400

    Removed config/db Heroku name and added dj5k
    Added dj5k-backend back into config/db as there are issues with Curl

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'thawing-beyond-32492'
+const mongooseBaseName = 'dj5k-backend'
 
 // create the mongodb uri for development and test
 const database = {

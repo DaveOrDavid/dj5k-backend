95b16bac81ee57f8e0f369e457c7792618f915a2
DaveOrDavid <david.santore@gmail.com>
2019-07-22 13:02:33 -0400

    Changed Heroku host info thawing-beyond-32492
    Changed Heroku host info thawing-beyond-32492

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'dj5k-backend'
+const mongooseBaseName = 'thawing-beyond-32492'
 
 // create the mongodb uri for development and test
 const database = {
